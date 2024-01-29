3a8ae20208d64f70c60799f3842f7b9d3c6b0576
Ayush Gupta <86952522+NotSlypher@users.noreply.github.com>
2024-01-29 12:20:26 +0530

    assignment 1

---
+++ 01-Fundamentals-Part-1/starter/assignment1.js
@@ -0,0 +1,5 @@
+let continent = 'Asia';
+let country = 'India';
+let population = 1800
+
+console.log("my country is " + country + " in the continent " + continent + ' with population of ' + population + ' millions.'); 
