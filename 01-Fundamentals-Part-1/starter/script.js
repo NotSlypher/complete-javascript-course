72fd4b3da5065213cb9de659866c5382ae742dd7
Ayush Gupta <86952522+NotSlypher@users.noreply.github.com>
2024-01-29 11:57:25 +0530

    Varianles and values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,14 @@
 let js = 'amazing';
-if (js === 'amazing')
-    alert('js is fun');
-
 console.log(34 + 234 + 23);
+
+console.log('Jonas');
+console.log(43)
+
+let firstName = 'Ayush';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let person = "Ayush";
+let PI = 3.14;
+
